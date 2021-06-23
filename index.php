3783332faa344523b67f559704b62f1286eff926
Fernando Fraga <fernando2015fraga@gmail.com>
2021-06-23 16:37:12 -0300

    [Upload] Banner READMD

--- index.php
+++ index.php
@@ -1,60 +1,3 @@
-<?php
-
-require __DIR__."/vendor/autoload.php";
-
-use Src\Tools\Table;
-
-$data = [
-    0 => [
-        "id"    => 14,
-        "name"  => "Fernando",
-        "email" => "fernando@gmail.com"
-    ],
-    1 => [
-        "id"    => 15,
-        "name"  => "Bruno",
-        "email" => "bruno@gmail.com"
-    ],
-    2 => [
-        "id"    => 16,
-        "name"  => "Maria",
-        "email" => "maria@gmail.com"
-    ],
-    3 => [
-        "id"    => 18,
-        "name"  => "Stefany",
-        "email" => "stefany@gmail.com"
-    ]
-];
-
-$table = new Table([
-    "id" => "people",
-    "itemId" => "id",
-    "align" => "left",
-    "tdId" => false,
-]);
-
-$table->setHeader([
-    [
-        "content" => "Id",
-        "align" => "center",
-    ],
-    "Nome Completo",
-    "Email"
-]); 
-
-$table->setBody($data, [
-    [
-        "content" => "id",
-        "align" => "center"
-    ],
-    "name",
-    "email"
-    ]
-);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,7 +8,9 @@ $table->setBody($data, [
     <title>Document</title>
 </head>
 <body>
-    <?= $table->print(); ?>
+    <div class="switch-btn">
+        <div class="switch-indice"></div>
+    </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </html>
